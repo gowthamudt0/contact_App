d9aab2cd5c1748560e146075aeae57bfc47a624f
Gowtham S <111949803+gowthamudt0@users.noreply.github.com>
2024-03-12 13:33:49 +0530

    Delete server directory

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require("dotenv").config();
-const app = require("./app");
-const db = require("./config/database");
-
-db();
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}..`);
-});
